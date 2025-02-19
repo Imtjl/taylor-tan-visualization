6df1817fe0d3299b80cb1f954d1d7fb94b52f2e8
worthant <b_dvorkin@niuitmo.ru>
2025-02-19 19:06:25 +0300

    :test_tube: feat(test): Cover factorial function with module tests

---
+++ test/math.test.ts
@@ -0,0 +1,16 @@
+import { factorial } from '../src/math';
+
+describe('factorial', () => {
+    test('should return 1 for 0 and 1', () => {
+        expect(factorial(0)).toBe(1);
+        expect(factorial(1)).toBe(1);
+    });
+
+    test('should correctly calculate factorials', () => {
+        expect(factorial(2)).toBe(2);
+        expect(factorial(3)).toBe(6);
+        expect(factorial(4)).toBe(24);
+        expect(factorial(5)).toBe(120);
+        expect(factorial(12)).toBe(479001600);
+    });
+});
